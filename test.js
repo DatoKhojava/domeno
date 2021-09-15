2b4da11ddb07d415b3f0fec6f8fad33bacdd75d5
DatoKhojava <khojavad30@gmail.com>
2021-09-15 20:41:37 +0400

    quick sort module

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-import recursionSum from "./recursionSum.js";
+// import recursionSum from "./recursionSum.js";
+import quickSort from "./quickSort.js";
 
-console.log(recursionSum([1, 2, 3]));
+console.log(quickSort([2, 4, 5, 8, 3, 15, 11, 1, 1, 2]));
